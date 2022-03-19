da3944ea4673deb4641d690b3082fdc4ada40367
praveen-0208 <praveenkumaresan02@gmail.com>
2022-03-19 09:24:50 +0530

    smaple

--- detect-secrets-test/another_secret.py
+++ detect-secrets-test/another_secret.py
@@ -1,9 +1,13 @@
-red_herring = 'DEADBEEF'
-id = 'YW1pYWx3YXlzZ2VuZXJhdGluZ3BheWxvYWRzd2hlbmltaHVuZ3J5b3JhbWlhbHdheXNodW5ncnk'
+#!/usr/bin/python
+# Will change this later.
+SUPER_SEECRET_VALUE = 'c3VwZXIgbG9uZyBzdHJpbmcgc2hvdWxkIGNhdXNlIGVub3VnaCBlbnRyb3B5'
+VERY_SECRET_TOO = 'f6CGV4aMM9zedoh3OUNbSakBymo7yplB'  # pragma: allowlist secret
 
-base64_secret = 'c2VjcmV0IG1lc3NhZ2Ugc28geW91J2xsIG5ldmVyIGd1ZXNzIG15IHBhc3N3b3Jk'
-hex_secret = '8b1118b376c313ed420e5133ba91307817ed52c2'
-basic_auth = 'http://username:whywouldyouusehttpforpasswords@example.com'
 
-aws_access_key = 'AKIAIOSFODNN7EXAMPLE'
-aws_secret_access_key = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+def main():
+    print('Hello world!')
+
+
+if __name__ == '__main__':
+    main()
+    
